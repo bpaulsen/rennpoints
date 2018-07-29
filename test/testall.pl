20bf5a88550f0ddf1a57ea7b627f9923790bc583
Brian Paulsen <brian@thepaulsens.com>
2018-07-28 23:22:17 -0400

    set up to run a single test

--- test/testall.pl
+++ test/testall.pl
@@ -5,4 +5,5 @@ use Cwd qw(abs_path);
 use File::Basename qw(dirname);
 use Test::Class::Moose::Load File::Spec->catfile( dirname(abs_path($0)), 't' );
 use Test::Class::Moose::Runner;
-Test::Class::Moose::Runner->new->runtests;
+
+Test::Class::Moose::Runner->new(test_classes => \@ARGV)->runtests;
