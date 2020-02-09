c17e906be24961478dad3e581eef0154e42abc46
Brian Paulsen <brian@thepaulsens.com>
2020-02-09 09:23:43 -0500

    removed import of unused functions

--- public_html/laptimes/getFromClubReg2.pl
+++ public_html/laptimes/getFromClubReg2.pl
@@ -2,7 +2,7 @@
 
 use warnings;
 use strict;
-use RennPoints qw( getDBConnection getAllClassRecords getLapTimePrediction formatTime insertRacerIntoDB );
+use RennPoints qw( getDBConnection );
 use LWP::UserAgent;
 use HTTP::Cookies;
 use HTML::Form;
