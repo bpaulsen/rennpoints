ee1dc6e9eb144d311695f3030720b610bcdde7f5
Brian Paulsen <brian@thepaulsens.com>
2022-10-13 02:54:34 +0000

    fixed typo of month

--- lib/RennPoints/ClubRacing.pm
+++ lib/RennPoints/ClubRacing.pm
@@ -15,7 +15,7 @@ has '_config' => ( is => 'ro', builder => '_build_config', lazy => 1 );
 my %MONTHS = ( Jan => "01", Feb => "02", Mar => "03", Apr => "04", May => "05", Jun => "06",
                Jul => "07", Aug => "08", Sep => "09", Oct => "10", Nov => "11", Dec => "12",
 	       January => "01", February => "02", March => "03", April => "04", June => "06",
-	       July => "07", August => "08", Septemeber => "09", October => "10",
+	       July => "07", August => "08", September => "09", October => "10",
 	       November => "11", December => "12"
            );
 
