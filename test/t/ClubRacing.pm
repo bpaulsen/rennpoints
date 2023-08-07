bdbb1ad0b568c491651f2e084994d5aaf06b66cd
Brian Paulsen <brian@thepaulsens.com>
2023-08-07 03:20:27 +0000

    limiting number of races

--- test/t/ClubRacing.pm
+++ test/t/ClubRacing.pm
@@ -2,6 +2,7 @@ package TestsFor::RennPoints::ClubRacing;
 use Test::Class::Moose;
 use RennPoints::ClubRacing;
 use File::Slurp qw(read_file);
+use Data::Dumper;
 
     # methods that begin with test_ are test methods.
 sub test_parse_events {
@@ -36,9 +37,25 @@ sub test_parse_events4 {
 
     isa_ok my $clubracing = RennPoints::ClubRacing->new( content => scalar(read_file("test_files/clubracing/clubracing_4")) ), 'RennPoints::ClubRacing';
 
-    is @{$clubracing->events}, 21, "Testing number of events that were parsed";
+    is @{$clubracing->events}, 20, "Testing number of events that were parsed";
     is $clubracing->events->[0]->{description}, '2022-09 Thunderhill', 'Check description of first race';
 }
+
+sub test_parse_events5 {
+    my $test = shift;
+
+    isa_ok my $clubracing = RennPoints::ClubRacing->new( content => scalar(read_file("test_files/clubracing/clubracing_5")) ), 'RennPoints::ClubRacing';
+
+    is @{$clubracing->events}, 15, "Testing number of events that were parsed";
+    my ( $clubracing_event ) = grep { $_->{description} eq '2023-07 Brainerd' } @{$clubracing->events};
+
+    is $clubracing_event->{description}, '2023-07 Brainerd', 'Check description of second race';
+    is @{$clubracing_event->{event}->races}, 3, "Testing number of events that were parsed";
+
+#    my $races = $clubracing_event->{event}->races;
+
+}
+
 
 sub test_url {
     my $test = shift;
